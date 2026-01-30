c2b3f6d64755d7ff9d9068e764b94074c3119674
shallowaria <shallowaria@gmail.com>
2026-01-30 20:12:53 +0800

    achieve slide

--- lib/components/Home/kb_slider.dart
+++ lib/components/Home/kb_slider.dart
@@ -1,20 +1,49 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:kayb_shop/viewmodels/home.dart';
 
 class KbSlider extends StatefulWidget {
-  const KbSlider({super.key});
+  final List<BannerItem> bannerList;
+  const KbSlider({super.key, required this.bannerList});
 
   @override
   KbSliderState createState() => KbSliderState();
 }
 
 class KbSliderState extends State<KbSlider> {
+  Widget _getSlider() {
+    // 获取屏幕宽度
+    final double screenWidth = MediaQuery.of(context).size.width;
+    // 返回轮播图插件
+    // 根据数据渲染不同的轮播对象
+    return CarouselSlider(
+      items: List.generate(
+        widget.bannerList.length,
+        (int index) => Image.network(
+          widget.bannerList[index].imgUrl, //父传子
+          fit: BoxFit.cover,
+          width: screenWidth,
+        ),
+      ),
+      options: CarouselOptions(
+        autoPlayInterval: Duration(seconds: 5),
+        height: 300,
+        viewportFraction: 1.0,
+        autoPlay: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: Text('轮播图', style: TextStyle(fontSize: 20, color: Colors.white)),
-    );
+    // Stack -> 轮播图 搜索框 指示灯导航
+    return Stack(children: [_getSlider()]);
+
+    // return Container(
+    //   height: 300,
+    //   color: Colors.blue,
+    //   alignment: Alignment.center,
+    //   child: Text('轮播图', style: TextStyle(fontSize: 20, color: Colors.white)),
+    // );
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:kayb_shop/components/Home/kb_hot.dart';
 import 'package:kayb_shop/components/Home/kb_more_list.dart';
 import 'package:kayb_shop/components/Home/kb_slider.dart';
 import 'package:kayb_shop/components/Home/kb_suggestion.dart';
+import 'package:kayb_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,11 +14,23 @@ class HomeView extends StatefulWidget {
 }
 
 class HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: '1',
+      imgUrl: 'https://seopic.699pic.com/photo/50075/6521.jpg_wh1200.jpg',
+    ),
+    BannerItem(
+      id: '2',
+      imgUrl: 'https://seopic.699pic.com/photo/50118/7084.jpg_wh1200.jpg',
+    ),
+    BannerItem(id: '3', imgUrl: 'https://p7.qhimg.com/t0190c170fcad30e29b.png'),
+  ];
+
   // 获取滚动容器的内容
   List<Widget> _getScrollChildren() {
     return [
       // 包括普通 Winget 的 sliver 家族的组件
-      SliverToBoxAdapter(child: KbSlider()),
+      SliverToBoxAdapter(child: KbSlider(bannerList: _bannerList)),
       //SliverGrid 和 SliverList只能纵向排列
       // 放置分类组件
       SliverToBoxAdapter(child: SizedBox(height: 10)),
